b0daf0efd1f4d48f6a9bf8fdeed31b8011beeb2e
jrrohrer <jessie.rohrer@gmail.com>
2021-06-21 14:38:05 -0400

    commit tree 78ca53392feb2204d5f8801542dc43000ee137fb

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+  input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+}); 
 }
+
+// Let's review the parts of this code:
+
+// The node that will be doing the listening. We store that node in the input constant
+// The invocation of addEventListener on the node that will be doing the listening
+// The first argument is the event name to listen for
+// The second argument is the callback function. It's work that will be executed when the node "hears" the event
